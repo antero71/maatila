5d4735581d72c9941ab7703ad3e89b2a6f471413
anterooikkonen <anterooikkonen@192.168.33.119>
2014-06-22 15:18:06 +0300

    getKoko -metodi lisätty

--- src/main/java/com/antero/maatilasimulaattori/domain/Kanala.java
+++ src/main/java/com/antero/maatilasimulaattori/domain/Kanala.java
@@ -12,13 +12,14 @@ import java.util.Collection;
  *
  * @author Antero Oikkonen
  */
-public class Kanala extends MaatilanOsa {
+public class Kanala extends MaatilanOsa implements Varastoitava {
 
     private int kanalanKoko = 5;
     private Collection<Kana> kanat = new <Kana>ArrayList();
 
     public Kanala(String nimi) {
         super(nimi);
+        setHinta(100);
     }
 
     public int munita() {
@@ -53,6 +54,11 @@ public class Kanala extends MaatilanOsa {
 
     public int getKanalanKoko() {
         return kanalanKoko;
+    }
+
+    @Override
+    public int getKoko() {
+       return kanalanKoko;
     }
 
 }

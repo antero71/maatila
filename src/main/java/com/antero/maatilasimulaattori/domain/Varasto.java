5fe6b79d48f1336ca44d4a9cc103f41993332207
anterooikkonen <anterooikkonen@192.168.33.119>
2014-06-22 17:20:40 +0300

    annaTuote(Varastoitava tuote, int rahaa) tarkistaa nyt vain että sman luokan instanssi löytyy

--- src/main/java/com/antero/maatilasimulaattori/domain/Varasto.java
+++ src/main/java/com/antero/maatilasimulaattori/domain/Varasto.java
@@ -8,6 +8,7 @@ package com.antero.maatilasimulaattori.domain;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Iterator;
+import java.util.List;
 
 /**
  *
@@ -15,7 +16,7 @@ import java.util.Iterator;
  */
 public class Varasto extends MaatilanOsa {
 
-    private final Collection<Varastoitava> tuotteet = new ArrayList<>();
+    private final List<Varastoitava> tuotteet = new ArrayList<>();
 
     private int tilavuus = 50; // oletustilavuus
 
@@ -25,7 +26,7 @@ public class Varasto extends MaatilanOsa {
     }
 
     /**
-     * palauttaa <code>Valmistusaineet</code> jos kaikki aineet löytyy ja 
+     * palauttaa <code>Valmistusaineet</code> jos kaikki aineet löytyy ja
      * poistaa ne varastosta. muuten palauttaa null
      */
     public Valmistusaineet annaValmistusaineet(Valmistusaineet aineet) {
@@ -60,6 +61,12 @@ public class Varasto extends MaatilanOsa {
             tuotteet.add(tuote);
         } else {
             throw new IllegalArgumentException("Varastoon ei mahdu tuotetta jonka koko on " + tuote.getKoko());
+        }
+    }
+
+    public void addTuote(Varastoitava tuote, int maara) {
+        for (int i = 0; i < maara; i++) {
+            addTuote(tuote);
         }
     }
 
@@ -98,4 +105,58 @@ public class Varasto extends MaatilanOsa {
         }
         return varastonkoko;
     }
+
+    /**
+     * jos tuote löytyy, palautetaan true ja poistetaan tuote varastosta,
+     * muutoin palautetaan false
+     */
+    public boolean annaTuote(Varastoitava tuote) {
+        if (this.tuotteet.contains(tuote)) {
+            tuotteet.remove(tuote);
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * jos tuote löytyy ja rahaa riittää palautetaan tuote ja poistetaan tuote
+     * varastosta jos<code>tuote</code> on <code>null</code> tai rahaa on
+     * vähemmän kuin 1 heittää <code>IllegalArgumentException</code> poikkeuksen
+     *
+     * @param tuote
+     * @param rahaa
+     * @return <code>null</code> jos ei löydy, muuten Varastoitava objektin
+     */
+    public Varastoitava annaTuote(Varastoitava tuote, int rahaa) throws IllegalArgumentException {
+        if (tuote == null || rahaa < 1) {
+            throw new IllegalArgumentException("Tuote ei saa olla null ja rahamäärän pitää olla suurempi kuin 0");
+        }
+
+        Iterator<Varastoitava> iter = tuotteet.iterator();
+
+        Varastoitava t = null;
+
+        Varastoitava loytyi = null;
+
+        while (iter.hasNext()) {
+            t = iter.next();
+            if (t.getClass() == tuote.getClass()) {
+                loytyi = t;
+                break;
+            }
+        }
+        if (loytyi != null) {
+            tuotteet.remove(loytyi);
+            return t;
+        }
+
+        return null;
+    }
+
+    public void addTuotteet(Collection<Varastoitava> lisattavattuotteet) {
+        for (Varastoitava tuote : lisattavattuotteet) {
+            addTuote(tuote);
+        }
+    }
+
 }
